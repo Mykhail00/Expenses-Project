ddae1dcd53097ef181f85b2dd80c8d014c9fee75
Mykhail00 <bazhenovm000@gmail.com>
2023-12-04 19:15:48 +0200

    attached user to requests and refactored user authentication

---
+++ app/Middleware/AuthenticateMiddleware.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Contracts\AuthInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class AuthenticateMiddleware implements MiddlewareInterface
+{
+    public function __construct(public readonly AuthInterface $auth)
+    {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return $handler->handle($request->withAttribute('user', $this->auth->user()));
+    }
+}

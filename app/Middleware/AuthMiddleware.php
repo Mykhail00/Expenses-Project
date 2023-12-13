c8e6121879d7c121a40a0c6eab1e4b7999ac95ba
Mykhail00 <bazhenovm000@gmail.com>
2023-12-13 18:09:08 +0200

    added categories crud operations

--- app/Middleware/AuthMiddleware.php
+++ app/Middleware/AuthMiddleware.php
@@ -9,19 +9,22 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Views\Twig;
 
 class AuthMiddleware implements MiddlewareInterface
 {
     public function __construct(
         public readonly ResponseFactoryInterface $responseFactory,
         public readonly AuthInterface $auth,
-        public readonly SessionInterface $session
+        public readonly Twig $twig
     ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         if ($user = $this->auth->user()) {
+            $this->twig->getEnvironment()->addGlobal('auth', ['id' => $user->getId(), 'name' => $user->getName()]);
+
             return $handler->handle($request->withAttribute('user', $user));
         }
 
